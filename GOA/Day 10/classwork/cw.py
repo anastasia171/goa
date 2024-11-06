023ad32c9b9af30d262da29ee3ef66a743037afa
anastasia171 <anastasiakhakhiauri@gmail.com>
2024-11-06 23:06:04 +0400

    Add files via upload

---
+++ GOA/Day 10/classwork/cw.py
@@ -0,0 +1,17 @@
+#input - შეტანა / შეყვანა
+#output - გამოყვანა / გამოტანა
+
+
+# print(xelou world)
+
+# user_name = input("enter your name: ")
+
+
+# print("hello + user_name")
+
+
+#1)მომხმარებელს შეეკითხეთ სახელი შემდეგ კი მას მიესალმეთ ქართულად გამარჯობა სახელი
+
+user_name = input("თვენი სახელი: ")
+
+print("გამარჯობა " + user_name )
---
+++ GOA/Day 10/homework/hw3.py
@@ -0,0 +1,8 @@
+# ორი რიცხვის გამოკლება
+# დაწერე პროგრამა, რომელიც შემოატანინებს მომხმარებელს ორი რიცხვს და დაბეჭდავს მათ სხვაობას
+
+num1 = input("choose a devidend: " )
+num2 = input("choose a devisor: ")
+
+print(int(num1) / int(num2))
+
